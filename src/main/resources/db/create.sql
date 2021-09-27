3ce3f0802167249058e6d751fd96d73f8f28d15c
lanarokip <lankipkoecharo@gmail.com>
2021-09-27 09:11:07 +0300

    added the table once droped

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE company;
 \c company
 --int departid, String departmentName , int No_workers
-CREATE TABLE if NOT EXISTS departments(id SERIAL PRIMARY KEY,departmentName VARCHAR, NoWorkers VARCHAR);
---CREATE TABLE if NOT EXISTS users(userid SERIAL PRIMARY KEY ,position VARCHAR, role VARCHAR, departmentid VARCHAR);
---CREATE TABLE if NOT EXISTS news(id SERIAL PRIMARY KEY, general_News VARCHAR, departMent_News VARCHAR, departmentid INTEGER);
+CREATE TABLE if NOT EXISTS departments(id SERIAL PRIMARY KEY,departmentName VARCHAR, NoWorkers INTEGER);
+CREATE TABLE if NOT EXISTS users(id SERIAL PRIMARY KEY ,position VARCHAR, role VARCHAR, departmentid INTEGER);
+CREATE TABLE if NOT EXISTS news(id SERIAL PRIMARY KEY, General_News VARCHAR, DepartMent_News VARCHAR);
 --CREATE TABLE if NOT EXISTS news_department(id SERIAL PRIMARY KEY,departmentid INTEGER, newsid INTEGER );
