318a023c8b36e06d85641a1a0279c6f5bc7b1dd4
lanarokip <lankipkoecharo@gmail.com>
2021-09-27 10:53:08 +0300

    added the gitignore section

--- src/main/java/Sql2o/sql2oNews.java
+++ src/main/java/Sql2o/sql2oNews.java
@@ -1,4 +1,73 @@
 package Sql2o;
 
-public class sql2oNews {
+import dao.IntNews;
+import models.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class sql2oNews implements IntNews {
+
+    private final Sql2o sql2o;
+
+    public sql2oNews(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void create(News news) {
+        String sql = "INSERT INTO news(general_News,department_News) VALUES (:general_News,:department_news)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setId(id);
+        } catch (
+                Sql2oException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<News> findAll() {
+        try( Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM News")
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public News findbyId(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * From news WHERE id=:id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(News.class);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        String sql = "DELETE FROM news";
+            try (Connection con = sql2o.open()) {
+                con.createQuery(sql)
+                        .executeUpdate();
+            } catch (Sql2oException e) {
+                System.out.println(e);
+            }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE FROM news WHERE id=:id";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql).addParameter("id",id)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println("runnn");
+        }
+    }
+
 }
--- src/main/java/Sql2o/sql2oUser.java
+++ src/main/java/Sql2o/sql2oUser.java
@@ -1,4 +1,72 @@
 package Sql2o;
 
-public class sql2oUser {
+import dao.IntUser;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class sql2oUser implements IntUser {
+
+    private final Sql2o sql2o;
+
+    public sql2oUser(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+
+    public void create(User user) {
+        String sql ="INSERT INTO user(position,role,department) VALUES (:position,:role,:department)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql,true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM users")
+                    .executeAndFetch(User.class);
+        }
+    }
+
+    @Override
+    public User findbyId(int id) {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM users WHERE id=:id")
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(User.class);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        String sql = "DELETE FROM Users";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE FROM users WHERE id=:id";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql).addParameter("id",id)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println("runnn");
+        }
+    }
 }
