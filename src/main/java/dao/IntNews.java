87fc4a34e6f7b6ca480ed6b7da69d72e76b0ff74
lanarokip <lankipkoecharo@gmail.com>
2021-09-27 09:13:13 +0300

    added the inNews section class

--- src/main/java/dao/IntNews.java
+++ src/main/java/dao/IntNews.java
@@ -8,14 +8,16 @@ import java.util.List;
 public interface IntNews {
 
     //    CRUDE
-
+//create
     void create(News news);
-    void  addNewsToRestaurant(News news, Departments departments);
+//    void  addNewsToRestaurant(News news, Departments departments);
 
+    //read
     List<News> findAll();
-    List<Departments> getallDepartmentNews(int id);
-    News findbyId();
+//    List<Departments> getallDepartmentNews(int id);
+    News findbyId(int id);
 
+//    delete
     void deleteAll();
     void deleteById(int id);
 
